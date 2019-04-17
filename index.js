dd56ce2eac50dad6929f4b2bef3e474d65f1a3f1
Vishnu <vishnunroshan@bionworks.com>
2019-04-17 10:42:27 +0530

    some headers added

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-
-app.use(helmet())
-
+const expectCt = require('expect-ct');
+app.disable('x-powered-by');
+app.use(helmet());
+// app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 app.use(helmet.xssFilter());
 app.use(helmet.permittedCrossDomainPolicies());
 app.use(helmet.noSniff());
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.referrerPolicy({policy: 'same-origin'}));
 app.use(helmet.frameguard({action: 'deny'}));
-
+app.use(helmet.hsts({maxAge: 63072000, includeSubDomains: true, preload: true}));
+app.use(helmet.ieNoOpen());
+app.use(expectCt({
+	enforce: false,
+	maxAge: 123
+}));
 module.exports = app;
